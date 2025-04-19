14d322118acb47c8b6671e913d3280f6e3f8d941
Erty <erty.gamedev@gmail.com>
2025-04-19 23:27:50 +0200

    Utility functions for string split, trim, parse bool and replace token

--- common/src/utils.cpp
+++ common/src/utils.cpp
@@ -17,3 +17,33 @@ std::string M2PUtils::toUpperCase(std::string str)
 		});
 	return str;
 }
+
+std::vector<std::string> M2PUtils::split(std::string& str, const char delimiter)
+{
+	std::istringstream strStream{ str };
+	std::vector<std::string> segments;
+	std::string segment;
+	while (std::getline(strStream, segment, delimiter))
+	{
+		segments.push_back(segment);
+	}
+	return segments;
+}
+
+void M2PUtils::trim(std::string& str, const char* trim)
+{
+	str.erase(0, str.find_first_not_of(trim));
+	str.erase(str.find_last_not_of(trim) + 1);
+}
+
+bool M2PUtils::strToBool(std::string str)
+{
+	str = toLowerCase(str);
+	return !str.starts_with('n') && !str.starts_with('f') && str != "0";
+}
+
+void M2PUtils::replaceToken(std::string& str, const std::string& token, const std::string& value)
+{
+	while (str.find(token) != std::string::npos)
+		str.replace(str.find(token), token.length(), value);
+}
--- common/src/utils.h
+++ common/src/utils.h
@@ -1,13 +1,20 @@
 #pragma once
 
 #include <string>
+#include <sstream>
 #include <algorithm>
 #include <vector>
 
 namespace M2PUtils
 {
+	static inline const char* c_DEFAULT_TRIM = " \t";
+
 	std::string toLowerCase(std::string);
 	std::string toUpperCase(std::string);
+	std::vector<std::string> split(std::string&, const char = ' ');
+	void trim(std::string&, const char* = c_DEFAULT_TRIM);
+	bool strToBool(std::string);
+	void replaceToken(std::string&, const std::string&, const std::string&);
 
 	template<typename T>
 	bool contains(std::vector<T> vect, T needle)
