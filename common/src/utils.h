52e525abe2793f1f0b0c00e79272f8b4adb2fdef
Erty <erty.gamedev@gmail.com>
2025-04-21 23:15:25 +0200

    Utility function for extending vector

--- common/src/utils.h
+++ common/src/utils.h
@@ -11,14 +11,21 @@ namespace M2PUtils
 
 	std::string toLowerCase(std::string);
 	std::string toUpperCase(std::string);
-	std::vector<std::string> split(std::string&, const char = ' ');
-	void trim(std::string&, const char* = c_DEFAULT_TRIM);
-	bool strToBool(std::string);
-	void replaceToken(std::string&, const std::string&, const std::string&);
+	std::vector<std::string> split(std::string& str, const char delimiter = ' ');
+	void trim(std::string& str, const char* trim = c_DEFAULT_TRIM);
+	bool strToBool(std::string str);
+	void replaceToken(std::string& str, const std::string& token, const std::string& value);
 
 	template<typename T>
 	bool contains(std::vector<T> vect, T needle)
 	{
 		return std::find(vect.begin(), vect.end(), needle) != vect.end();
+	}
+
+	template<typename T>
+	void extendVector(std::vector<T>& first, const std::vector<T>& second)
+	{
+		first.reserve(second.size());
+		first.insert(first.end(), second.begin(), second.end());
 	}
 }
