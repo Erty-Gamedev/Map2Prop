88a44c80835bd2cb67ce2e41f6fda2e7972b7e7d
Erty <erty.gamedev@gmail.com>
2025-05-19 23:39:21 +0200

    Brush methods

--- formats/src/entity.cpp
+++ formats/src/entity.cpp
@@ -1,14 +1,89 @@
 #include <format>
 #include "entity.h"
+#include "utils.h"
+#include "wad3handler.h"
 
 using namespace M2PEntity;
+
+
+bool Brush::isToolBrush(ToolTexture toolTexture) const
+{
+	std::string textureName;
+	if (toolTexture == ToolTexture::BEVEL)
+		textureName = "bevel";
+	else if (toolTexture == ToolTexture::BOUNDINGBOX)
+		textureName = "boundingbox";
+	else if (toolTexture == ToolTexture::CLIP)
+		textureName = "clip";
+	else if (toolTexture == ToolTexture::CLIPBEVEL)
+		textureName = "clipbevel";
+	else if (toolTexture == ToolTexture::CONTENTWATER)
+		textureName = "contentwater";
+	else if (toolTexture == ToolTexture::ORIGIN)
+		textureName = "origin";
+
+	for (const auto& face : faces)
+	{
+		if (M2PUtils::toLowerCase(face.texture.name) != textureName)
+			return false;
+	}
+	return true;
+}
+
+bool Brush::hasContentWater() const
+{
+	for (const auto& face : faces)
+	{
+		if (M2PUtils::toLowerCase(face.texture.name) == "contentwater")
+			return true;
+	}
+	return false;
+}
+
+std::pair<M2PGeo::Vector3, M2PGeo::Vector3> Brush::getBounds() const
+{
+	M2PGeo::Vector3 min = faces[0].vertices[0].coord();
+	M2PGeo::Vector3 max = faces[0].vertices[0].coord();
+
+	for (const auto& face : faces)
+	{
+		if (M2PWad3::Wad3Handler::isSkipTexture(face.texture.name))
+			continue;
+
+		for (const auto& vertex : face.vertices)
+		{
+			if (vertex.x < min.x)
+				min.x = vertex.x;
+			if (vertex.y < min.y)
+				min.y = vertex.y;
+			if (vertex.z < min.z)
+				min.z = vertex.z;
+
+			if (vertex.x > max.x)
+				max.x = vertex.x;
+			if (vertex.y > max.y)
+				max.y = vertex.y;
+			if (vertex.z > max.z)
+				max.z = vertex.z;
+		}
+	}
+
+	return std::make_pair(min, max);
+}
+
+M2PGeo::Vector3 Brush::getCenter() const
+{
+	std::pair<M2PGeo::Vector3, M2PGeo::Vector3> bounds = getBounds();
+	return (bounds.first + bounds.second) / 2;
+}
+
 
 Entity::Entity()
 {
 	raw.reserve(1024);
 }
 
-std::string M2PEntity::Entity::toString() const
+std::string Entity::toString() const
 {
 	std::string str;
 	str.reserve(raw.size());
@@ -22,7 +97,7 @@ std::string M2PEntity::Entity::toString() const
 	if (brushes.size() > 0)
 	{
 		str += "{\n";
-		for (M2PEntity::Brush const& brush : brushes)
+		for (Brush const& brush : brushes)
 		{
 			str += brush.raw;
 		}
