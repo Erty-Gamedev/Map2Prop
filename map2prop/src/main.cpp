86629971b1c22b6a43b2114fc0904dd2736e61a0
Erty <erty.gamedev@gmail.com>
2025-05-26 22:28:37 +0200

    Update main entrypoint

--- map2prop/src/main.cpp
+++ map2prop/src/main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
-#include <filesystem>
 #include "logging.h"
 #include "config.h"
+#include "geometry.h"
+#include "map_format.h"
+#include "export.h"
+
 
 using Logging::Logger;
+static Logger& logger = Logger::getLogger("map2prop");
 
 
 int main(int argc, char** argv)
@@ -11,8 +14,24 @@ int main(int argc, char** argv)
     M2PConfig::handleArgs(argc, argv);
     using M2PConfig::g_config;
 
-    if (!g_config.input.empty())
-        std::cout << g_config.input << std::endl;
+    if (g_config.eager)
+    {
+        M2PGeo::g_isEager = true;
+        logger.debug("Using eager algorithm");
+    }
 
-    std::cout << Logging::LogLevelName[Logging::loglevel_console] << std::endl;
+    try
+    {
+        M2PMap::MapReader mapReader = M2PMap::MapReader(g_config.inputFilepath, g_config.outputDir);
+
+        auto models = M2PExport::prepareModels(mapReader.entities, mapReader.wadHandler);
+        int res = M2PExport::processModels(models, mapReader.hasMissingTextures());
+
+        if (g_config.mapcompile && !res)
+            M2PExport::rewriteMap(mapReader.entities);
+    }
+    catch (const std::exception& e)
+    {
+        logger.error(e.what());
+    }
 }
