c5fd3aa592454ea52a9b30b9ce1b4a5e09df5147
Erty <erty.gamedev@gmail.com>
2025-05-30 11:00:05 +0200

    Helper functions for reading binary data

---
+++ common/src/binutils.cpp
@@ -0,0 +1,54 @@
+#include <vector>
+#include "binutils.h"
+
+using namespace M2PBinUtils;
+
+
+char M2PBinUtils::readChar(std::ifstream& file)
+{
+	char buffer{};
+	file.read(&buffer, sizeof(char));
+	return buffer;
+}
+
+void M2PBinUtils::readChar(std::ifstream& file, char* buffer)
+{
+	file.read(buffer, sizeof(char));
+}
+
+unsigned char M2PBinUtils::readByte(std::ifstream& file)
+{
+	unsigned char buffer{};
+	file.read(reinterpret_cast<char*>(&buffer), sizeof(unsigned char));
+	return buffer;
+}
+
+std::int32_t M2PBinUtils::readInt(std::ifstream& file)
+{
+    std::int32_t buffer{};
+    file.read(reinterpret_cast<char*>(&buffer), sizeof(std::int32_t));
+    return buffer;
+}
+
+float M2PBinUtils::readFloat(std::ifstream& file)
+{
+	float buffer{};
+	file.read(reinterpret_cast<char*>(&buffer), sizeof(float));
+	return buffer;
+}
+
+std::string M2PBinUtils::readNTString(std::ifstream& file, size_t length)
+{
+	if (length < 1)
+		return "";
+	std::vector<char> bytes;
+	bytes.assign(length, '\0');
+	file.read(reinterpret_cast<char*>(bytes.data()), length);
+	return std::string{ bytes.data() };
+}
+
+std::string M2PBinUtils::readLPString(std::ifstream& file)
+{
+	unsigned char length = readByte(file);
+	return readNTString(file, length);
+}
---
+++ common/src/binutils.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+
+namespace M2PBinUtils
+{
+	char readChar(std::ifstream& file);
+	void readChar(std::ifstream& file, char *buffer);
+	unsigned char readByte(std::ifstream& file);
+	std::int32_t readInt(std::ifstream& file);
+	float readFloat(std::ifstream& file);
+	std::string readNTString(std::ifstream& file, size_t length);
+	std::string readLPString(std::ifstream& file);
+}
