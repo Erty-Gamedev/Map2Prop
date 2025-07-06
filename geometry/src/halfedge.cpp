b905caf6c023dec0b4e47c85e8d42cbeffa6296a
Erty <erty.gamedev@gmail.com>
2025-07-06 19:57:41 +0200

    Initial commit of Half-Edge mesh structure class

---
+++ geometry/src/halfedge.cpp
@@ -0,0 +1,308 @@
+#include <set>
+#include "halfedge.h"
+
+
+using namespace M2PHalfEdge;
+
+
+M2PGeo::Vector3 Coord::coord()
+{
+	return { x, y, z };
+}
+
+
+Vertex::Vertex(std::shared_ptr<Coord>& coord, const M2PGeo::Vector2& _uv)
+{
+	position = coord;
+	uv = _uv;
+}
+
+bool Edge::operator==(const Edge& rhs) const
+{
+	return origin->coord() == rhs.origin->coord() && next->origin->coord() == rhs.next->origin->coord();
+}
+
+std::shared_ptr<Vertex> Face::getVertex(Coord coord) const
+{
+	for (int i = 0; i < 3; ++i)
+	{
+		if ((*vertices[i]->position).distance(coord) < 0.001)
+			return vertices[i];
+	}
+	return std::shared_ptr<Vertex>(nullptr);
+}
+
+M2PGeo::Vector3 Face::fullNormal() const
+{
+	auto& a = edge->origin;
+	auto& b = edge->next->origin;
+	auto& c = edge->next->next->origin;
+
+	return M2PGeo::segmentsCross(a->coord(), b->coord(), c->coord());
+}
+
+bool Face::operator<(const Face& rhs) const
+{
+	for (int i = 0; i < 3; ++i)
+		if (*vertices[i]->position != *rhs.vertices[i]->position)
+			return true;
+	return false;
+}
+
+bool M2PHalfEdge::Face::operator==(const Face& rhs) const
+{
+	for (int i = 0; i < 3; ++i)
+		if (*vertices[i]->position != *rhs.vertices[i]->position)
+			return false;
+	return flipped == rhs.flipped;
+}
+
+void SmoothFan::addFace(std::shared_ptr<Face>& face)
+{
+	faces.push_back(face);
+	auto faceNormal = face->fullNormal();
+	for (const M2PGeo::Vector3& normal : normals)
+		if (normal == face->normal)
+			return;
+	normals.push_back(face->normal);
+	accumulatedNormal += face->fullNormal();
+}
+
+void SmoothFan::applySmooth() const
+{
+	M2PGeo::Vector3 average = accumulatedNormal;
+
+	for (auto& face : faces)
+	{
+		for (int i = 0; i < 3; ++i)
+		{
+			if (face->vertices[i]->position->index == vertex->index)
+			{
+				face->vertices[i]->normal = accumulatedNormal.normalised();
+				break;
+			}
+		}
+	}
+}
+
+std::shared_ptr<Coord>& Mesh::addVertex(const M2PGeo::Vertex _vertex)
+{
+	for (std::shared_ptr<Coord>& vertex : vertices)
+	{
+		if (_vertex.coord().distance(*vertex) < M2PGeo::c_EPSILON_MERGE)
+			return vertex;
+	}
+	vertices.push_back(std::make_shared<Coord>(static_cast<unsigned int>(vertices.size()), _vertex));
+	return vertices.back();
+}
+
+void Mesh::findTwins(std::shared_ptr<Edge>& edge)
+{
+	std::shared_ptr<Coord>& origin = edge->origin;
+	std::shared_ptr<Coord>& end = edge->next->origin;
+
+	for (std::shared_ptr<Edge>& other : edges)
+	{
+		if (other->origin == end && other->next->origin == origin)
+		{
+			edge->twin = other;
+			other->twin = edge;
+			break;
+		}
+	}
+}
+
+void Mesh::addTriangle(const M2PGeo::Triangle& triangle, const M2PGeo::Vector3 normal, const M2PGeo::Texture& texture, bool flipped)
+{
+	faces.push_back(std::make_shared<Face>(static_cast<unsigned int>(faces.size()), normal, texture, flipped));
+	std::shared_ptr<Face> face = faces.back();
+
+	std::shared_ptr<Coord> v0 = addVertex(triangle.vertices[0]);
+	std::shared_ptr<Coord> v1 = addVertex(triangle.vertices[1]);
+	std::shared_ptr<Coord> v2 = addVertex(triangle.vertices[2]);
+
+	face->vertices[0] = std::make_shared<Vertex>(v0, triangle.vertices[0].uv);
+	face->vertices[1] = std::make_shared<Vertex>(v1, triangle.vertices[1].uv);
+	face->vertices[2] = std::make_shared<Vertex>(v2, triangle.vertices[2].uv);
+
+	edges.push_back(std::make_shared<Edge>(static_cast<unsigned int>(edges.size()), v0, face));
+	std::shared_ptr<Edge> e0 = edges.back();
+	edges.push_back(std::make_shared<Edge>(static_cast<unsigned int>(edges.size()), v1, face));
+	std::shared_ptr<Edge> e1 = edges.back();
+	edges.push_back(std::make_shared<Edge>(static_cast<unsigned int>(edges.size()), v2, face));
+	std::shared_ptr<Edge> e2 = edges.back();
+
+	face->edge = e0;
+
+	e0->next = e1; e0->prev = e2; v0->edge = e0;
+	e1->next = e2; e1->prev = e0; v1->edge = e1;
+	e2->next = e0; e2->prev = e1; v2->edge = e2;
+
+	findTwins(e0);
+	findTwins(e1);
+	findTwins(e2);
+}
+
+std::vector<std::shared_ptr<Edge>> Mesh::getEdgesByVertex(const Coord& vertex)
+{
+	std::vector<std::shared_ptr<Edge>> foundEdges;
+
+	for (auto& edge : edges)
+	{
+		if (*edge->origin == vertex || *edge->next->origin == vertex)
+			foundEdges.push_back(edge);
+	}
+
+	return foundEdges;
+}
+
+std::vector<std::shared_ptr<Face>> Mesh::getFacesByVertex(const Coord& vertex)
+{
+	std::vector<std::shared_ptr<Face>> foundFaces;
+
+	for (auto& edge : getEdgesByVertex(vertex))
+	{
+		if (edge->face)
+			foundFaces.push_back(edge->face);
+		if (edge->twin && edge->twin->face)
+			foundFaces.push_back(edge->twin->face);
+	}
+
+	return foundFaces;
+}
+
+void Mesh::markSmoothEdges(
+	FP smoothing,
+	const std::vector<M2PGeo::Bounds>& alwaysSmooth,
+	const std::vector<M2PGeo::Bounds>& neverSmooth)
+{
+	FP threshold = M2PGeo::deg2rad(smoothing);
+	std::set<unsigned int> visitedEdges;
+
+	for (std::shared_ptr<Edge>& edge : edges)
+	{
+		if (visitedEdges.find(edge->index) != visitedEdges.end())
+			continue;
+
+		visitedEdges.insert(edge->index);
+
+		if (!edge->face || !edge->twin || !edge->twin->face)
+			continue;
+
+		visitedEdges.insert(edge->twin->index);
+
+		if (!neverSmooth.empty()
+			&& M2PGeo::pointInBounds(edge->origin->coord(), neverSmooth)
+			&& M2PGeo::pointInBounds(edge->next->origin->coord(), neverSmooth)
+			)
+		{
+			continue;
+		}
+
+		if (!alwaysSmooth.empty()
+			&& M2PGeo::pointInBounds(edge->origin->coord(), alwaysSmooth)
+			&& M2PGeo::pointInBounds(edge->next->origin->coord(), alwaysSmooth)
+			)
+		{
+			edge->sharp = false;
+			edge->twin->sharp = false;
+			continue;
+		}
+
+		if (edge->face->normal.angle(edge->twin->face->normal) < threshold)
+		{
+			edge->sharp = false;
+			edge->twin->sharp = false;
+		}
+	}
+}
+
+static inline SmoothFan walkSmoothFan(
+	std::shared_ptr<Coord>& vertex, std::shared_ptr<Edge>& currentEdge,
+	std::set<unsigned int>& visitedFaces
+)
+{
+	SmoothFan fan{ vertex };
+
+	// If both edges from this vertex are sharp, this fan only contains this face
+	if (currentEdge->sharp && currentEdge->prev->sharp)
+	{
+		visitedFaces.insert(currentEdge->face->index);
+		fan.addFace(currentEdge->face);
+		return fan;
+	}
+
+	std::shared_ptr<Edge> startEdge = currentEdge;
+	bool backwards{ true };
+
+	int g = 0;
+	while (true)
+	{
+		if (g > 100)
+			throw std::runtime_error("loop detected");
+
+		if (visitedFaces.find(currentEdge->face->index) != visitedFaces.end())
+		{
+			backwards = false;
+			break;
+		}
+
+		visitedFaces.insert(currentEdge->face->index);
+		fan.addFace(currentEdge->face);
+
+		if (!currentEdge->twin || currentEdge->sharp)
+			break;
+
+		currentEdge = currentEdge->twin->next;
+		++g;
+	}
+
+	g = 0;
+	std::shared_ptr<Edge>& prevEdge = startEdge->prev->twin;
+	while (backwards && prevEdge)
+	{
+		if (g > 100)
+			throw std::runtime_error("loop detected");
+
+		if (visitedFaces.find(prevEdge->face->index) != visitedFaces.end())
+			break;
+
+		visitedFaces.insert(prevEdge->face->index);
+		fan.addFace(prevEdge->face);
+
+		if (!prevEdge->prev->twin || prevEdge->prev->twin->sharp)
+			break;
+
+		prevEdge = prevEdge->prev->twin;
+		++g;
+	}
+
+	return fan;
+}
+
+std::vector<SmoothFan> Mesh::getSmoothFansByVertex(std::shared_ptr<Coord>& vertex)
+{
+	std::vector<SmoothFan> fans;
+
+	std::set<unsigned int> visitedFaces;
+
+	std::shared_ptr<Edge> currentEdge = vertex->edge;
+
+	while (true)
+	{
+		if (visitedFaces.find(currentEdge->face->index) != visitedFaces.end())
+			break;
+
+		SmoothFan fan = walkSmoothFan(vertex, currentEdge, visitedFaces);
+		fan.applySmooth();
+
+		fans.push_back(fan);
+
+		if (!currentEdge->twin)
+			break;
+
+		currentEdge = currentEdge->twin->next;
+	}
+
+	return fans;
+}
---
+++ geometry/src/halfedge.h
@@ -0,0 +1,117 @@
+#pragma once
+#include <vector>
+#include "geometry.h"
+
+
+namespace M2PHalfEdge
+{
+	class Coord;
+	struct Edge;
+	struct Face;
+
+
+	class Coord : public M2PGeo::Vector3
+	{
+	public:
+		unsigned int index;
+		std::shared_ptr<Edge> edge;
+
+		Coord(unsigned int _index, const M2PGeo::Vertex vertex) : index(_index), M2PGeo::Vector3(vertex.coord()) {}
+
+		M2PGeo::Vector3 coord();
+	};
+
+	struct Vertex
+	{
+		std::shared_ptr<Coord> position;
+		M2PGeo::Vector3 normal;
+		M2PGeo::Vector2 uv;
+
+		Vertex(std::shared_ptr<Coord>& coord, const M2PGeo::Vector2& _uv);
+	};
+
+	struct Edge
+	{
+		unsigned int index;
+		std::shared_ptr<Coord> origin;
+		std::shared_ptr<Face> face;
+		std::shared_ptr<Edge> twin;
+		std::shared_ptr<Edge> next;
+		std::shared_ptr<Edge> prev;
+		bool sharp{ true };
+
+		Edge() = default;
+		Edge(
+			unsigned int _index,
+			std::shared_ptr<Coord>& _origin,
+			std::shared_ptr<Face>& _face
+		) : index(_index), origin(_origin), face(_face) {};
+
+		bool operator==(const Edge& rhs) const;
+	};
+
+	struct Face
+	{
+		unsigned int index;
+		std::shared_ptr<Edge> edge;
+		M2PGeo::Vector3 normal;
+		M2PGeo::Texture texture;
+		std::shared_ptr<Vertex> vertices[3];
+		bool flipped{ false };
+
+		Face() = default;
+		Face(
+			unsigned int _index,
+			const M2PGeo::Vector3 _normal,
+			const M2PGeo::Texture& _texture,
+			bool _flipped = false
+		) : index(_index), normal(_normal), texture(_texture), flipped(_flipped) {}
+
+		std::shared_ptr<Vertex> getVertex(Coord coord) const;
+		M2PGeo::Vector3 fullNormal() const;
+
+		bool operator<(const Face& rhs) const;
+		bool operator==(const Face& rhs) const;
+	};
+
+	struct SmoothFan
+	{
+		std::shared_ptr<Coord> vertex;
+		M2PGeo::Vector3 accumulatedNormal = M2PGeo::Vector3::zero();
+		std::vector<std::shared_ptr<Face>> faces;
+		std::vector<M2PGeo::Vector3> normals;
+
+		void addFace(std::shared_ptr<Face>& face);
+		void applySmooth() const;
+	};
+
+	struct Mesh
+	{
+		std::vector<std::shared_ptr<Coord>> vertices;
+		std::vector<std::shared_ptr<Edge>> edges;
+		std::vector<std::shared_ptr<Face>> faces;
+
+
+		std::shared_ptr<Coord>& addVertex(const M2PGeo::Vertex _vertex);
+
+		void findTwins(std::shared_ptr<Edge>& edge);
+
+		void addTriangle(
+			const M2PGeo::Triangle& triangle,
+			const M2PGeo::Vector3 normal,
+			const M2PGeo::Texture& texture,
+			bool flipped = false
+		);
+
+		std::vector<std::shared_ptr<Edge>> getEdgesByVertex(const Coord& vertex);
+
+		std::vector<std::shared_ptr<Face>> getFacesByVertex(const Coord& vertex);
+
+		void markSmoothEdges(
+			FP smoothing,
+			const std::vector<M2PGeo::Bounds>& alwaysSmooth,
+			const std::vector<M2PGeo::Bounds>& neverSmooth
+		);
+		std::vector<SmoothFan> getSmoothFansByVertex(std::shared_ptr<Coord>& vertex);
+	};
+}
